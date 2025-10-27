3737b875b477c025d6724e19951402511388a8cf
luyingyi818@outlook.com <yal52810>
2025-10-27 11:25:23 +0800

    修改替换api地址

--- src/common.rs
+++ src/common.rs
@@ -1024,7 +1024,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://149.104.0.224:21114()
+    "http://149.104.0.224:21114".to_owned()
 }
 
 #[inline]
